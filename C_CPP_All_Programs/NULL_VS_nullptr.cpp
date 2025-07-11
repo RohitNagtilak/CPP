a183478ae2d53da868fc2c6450e4b63b4f0401e0
Rohit Nagtilak <rohitnagtilak337@gmail.com>
2025-07-11 19:49:08 +0530

    Added new files

--- C_CPP_All_Programs/NULL_VS_nullptr.cpp
+++ C_CPP_All_Programs/NULL_VS_nullptr.cpp
@@ -1,4 +1,4 @@
-/*
+/* 
 Why is NULL Ambiguous?
 •	NULL is typically defined as 0 (an integer constant).
 •	When calling overloaded functions that accept both an integer and a pointer, passing NULL can confuse the compiler, as it matches both signatures.
@@ -7,13 +7,13 @@ Why is NULL Ambiguous?
 Why is nullptr Not Ambiguous ?
 •	nullptr (introduced in C++11) is a keyword representing a null pointer constant.
 •	It has its own type(std::nullptr_t) and only matches pointer overloads, not integers.
-•	This removes ambiguity and makes code safer and clearer.
+•	This removes ambiguity and makes code safer and clearer. 
 */
 
 #include <iostream>
 using namespace std;
 
-class Node
+class Node 
 {
 public:
     int data;
@@ -21,23 +21,20 @@ public:
     Node() : data(0), next(nullptr) {}
 };
 
-void func(int n)
+void func(int n) 
 {
     cout << "Called func(int)" << endl;
 }
 
-void func(Node* ptr)
+void func(Node* ptr) 
 {
     cout << "Called func(Node*)" << endl;
 }
 
-int main()
+int main() 
 {
-    func(NULL);    
-    // Error: ambiguous call in C++11 and later
-    
-    func(nullptr);
-    // Calls func(Node*), unambiguous
+    func(NULL);    // Error: ambiguous call in C++11 and later
+    func(nullptr);    // Calls func(Node*), unambiguous
 
     return 0;
 }
